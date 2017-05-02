89c63a20d0401609376cdf06003e9d32ea0b89ba
root <root@box-codeanywhere.com>
2017-05-02 17:29:04 -0400

    up

---
+++ dividindo.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+class dividindo{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+ 
+  
+  
+
+System.out.println("MMMMMMK00KKKKKXKKKKKKKKNKKKKMMMMMM");
+System.out.println("MMMMMW;:ccccccccccccccclcclcMMMMMM");
+System.out.println("MMMMMK;cccllllllllllllc:llc:WMMMMM");
+System.out.println("MMMMMk                      NMMMMM");
+System.out.println("MMMMMO                      XMMMMM");
+System.out.println("MMMMM0   entre com os       NMMMMM");
+System.out.println("MMMMM0     valores          NMMMMM");
+System.out.println("MMMMM0  a serem  divididos  NMMMMM");
+System.out.println("MMMMM0                      NMMMMM");
+System.out.println("MMMMM0                      NMMMMM");
+System.out.println("MMMMM0                      NMMMMM");
+System.out.println("MMMMMO                      NMMMMM");
+System.out.println("MMMMMO                      NMMMMM");
+System.out.println("MMMMMO                      NMMMMM");
+System.out.println("MMMMMO......................NMMMMM");
+System.out.println("MMMMMO......................NMMMMM\t\tTABUADA MALUCA - diviindo de um jeito diferente");
+System.out.println("MMMMMk......................NMMMMM");
+System.out.println("MMMMMk......................NMMMMM");
+System.out.println("MMMMMk......................NMMMMM");
+System.out.println("MMMMMk......................NMMMMM");
+System.out.println("MMMMMk......................NMMMMM");
+System.out.println("MMMMMk......................NMMMMM");
+System.out.println("MMMMMk......................NMMMMM");
+System.out.println("MMMMMk......................NMMMMM");
+System.out.println("MMMMMk......................NMMMMM");
+System.out.println("MMMMMx......................NMMMMM");
+System.out.println("MMMMMx......................NMMMMM");
+System.out.println("MMMMM0......................WMMMMM");
+System.out.println("MMMMMM0kkxxxxxdddddddddKddddMMMMMM");  
+  
+  int count=0; 
+  int m=sc.nextInt(); 
+  int n=sc.nextInt();
+ 
+ 
+//inicio 
+while(m>0){ 
+m-=n; 
+count++; 
+}
+  System.out.println("seu resultado foi="+count);
+
+
+}}//main function and class
